f2c70043673811c54935b79015297446a2dce703
ZanderBez <221205@virtualwindow.co.za>
2023-07-24 20:33:58 +0200

    update Browse page

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,31 @@ $(document).ready(function(){
 
     console.log("hello");
 
+    // -----------------------------------------------------------------------------------------------
+    // Home Page
+
+
     $(".hero-image".animate)({top:'-=100px'})
 
+    // -----------------------------------------------------------------------------------------------
+    // Browse Page
+
+    // Hide the description text from the plant card
+
+    $("#descriptionText").hide();
+
 });
+
+// When the plant cards is clicked
+
+$(".card").click(function(){
+
+    // toggle the price and description text
+    $("#descriptionText").toggle();
+
+
+    // Resize the plant image to fit additional content
+    $(".card-img-top").toggleClass("small");
+
+
+})
